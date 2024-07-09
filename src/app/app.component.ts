b310bb24538e8f12ea9a092dd4724cb73a0b24b9
NammiSanyasi <nammisanyasi4a@gmail.com>
2024-07-09 20:05:23 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'components-food';
+  
   img1 =
     'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Pizzas.png';
   img2 =
